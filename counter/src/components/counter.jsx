92ed2ca9a299a66aaf0311f67f4ad3395d042b2a
ShivamBhatnagar9987 <shivambhatnagar94@icloud.com>
2019-11-18 14:45:50 +0530

    incrementing, deleting and resetting the count
    description: the data can be incremented, reset and even the entire row can be deleted
    changes performed: removed the state and handled everything using props of the Counters component

--- counter/src/components/counter.jsx
+++ counter/src/components/counter.jsx
@@ -1,35 +1,28 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 class Counter extends Component {
-state={count:this.props.counter.value  ,
-  imageUrl:"https://picsum.photos/200"
-};
 styles={
   fontSize:'10px',
   fontWeight:"bold"
-}
-handleCount=()=>
-{
-  this.setState({count:this.state.count+1});
 }
   render() {
     return (
       <div>
       <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button onClick={this.handleCount} className="btn btn-secondary btn-sm">Increment</button>
+      <button onClick={()=>this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
 	  <button onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
     }
     formatCount()
     {
-      const {count} = this.state;
-      return count===0?"Zero":count;
+      const {value} = this.props.counter;
+      return value===0?"Zero":value;
     }
     getBadgeClasses()
     {
       let classes="badge m-2 badge-";
-      classes+=this.state.count===0?"warning":"primary";
+      classes+=this.props.counter.value===0?"warning":"primary";
       return classes;
     }
   }
--- counter/src/components/counters.jsx
+++ counter/src/components/counters.jsx
@@ -20,6 +20,21 @@ handleDelete=(counterId)=>
 	this.setState({counters:counter});
 
 }
+resetCount=()=>
+{
+	const counter=this.state.counters.map(c=>{
+	c.value=0;
+	return c;
+	});
+	this.setState({counters:counter});
+}
+handleIncrement=(counter)=>{
+	const counters=[...this.state.counters];
+	const index=counters.indexOf(counter);
+	counters[index]={...counter};
+	counters[index].value++;
+	this.setState({counters});
+}
 render()
 {
  return( 
@@ -27,7 +42,8 @@ render()
  <button onClick={this.resetCount} className="btn btn-primary btn-sm">Reset</button>
 { this.state.counters.map(counter=>
 <Counter 
-onDelete={this.handleDelete} 
+onDelete={this.handleDelete}
+onIncrement={this.handleIncrement}
 key={counter.id} 
 counter={counter}
 />)} 
